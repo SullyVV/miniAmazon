3bac16d117368ac5923d49f479aff7c277cdbcac
gh84@duke.edu <gh84@duke.edu>
2017-04-21 16:33:45 -0400

    addPack

--- client.py
+++ client.py
@@ -50,6 +50,8 @@ class Client():
             rsp_buffer.write(hdr_bytes[hdr_length:])
 
         # read the remaining message bytes
+        print(msg_length)
+        print(hdr_length)
         msg_length = msg_length - (4 - hdr_length)
         while msg_length > 0:
             rsp_bytes = self.sock.recv(min(8096, msg_length))
@@ -65,17 +67,37 @@ class Client():
 
     def AConnect(self):
         msg = amazon_pb2.AConnect()
-        msg.worldid = 1008
+        msg.worldid = 1000
         self.send(msg)
         self.recv()
 
     def process_AResponse(self) :
+        """
+         process response of Acommnads, store the information in database for future reference
+        """
+
         while (1):
             str = self.recv()
             if (len(str) > 0):
                 response = amazon_pb2.AResponses()
-                response.ParseFromString(str)
-                print(response)
+                try :
+                    response.ParseFromString(str)
+                    print(response)
+                    print(len(str))
+                except :
+                    print("error")
+
+    def AToPack(self, product_id, description, quantity, ship_id):
+        command = amazon_pb2.ACommands()
+        command.simspeed = 100000
+        pack = command.topack.add()
+        pack.whnum = 0
+        pack.shipid = ship_id;
+        pid = pack.things.add()
+        pid.id = product_id
+        pid.description = description
+        pid.count = quantity
+        self.send(command)
 
 
     def APurchase(self, product_id, description, quantity):
@@ -96,11 +118,12 @@ if __name__ == '__main__':
     client.connect()
     client.AConnect()
 
-    client.APurchase(1, "cake", 3)
-    client.APurchase(2, "apple", 4)
+    # client.APurchase(1, "cake", 3)
+    # client.APurchase(2, "apple", 4)
     threading.Thread(target=client.process_AResponse).start()
-    client.APurchase(3, "banana", 5)
-    client.APurchase(4, "orange", 6)
+    # client.APurchase(3, "banana", 5)
+    #client.APurchase(4, "orange", 6)
+    client.AToPack(3, "banana", 5, 1)
 
 
     while(1) :
